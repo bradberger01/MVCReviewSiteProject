4cb551d1aa02193b07c0117eac2ffe31a76bff6b
bradberger01 <bradberger@hotmail.com>
2017-10-19 20:03:02 -0400

    First Commit

---
+++ ReviewSiteProject2/Models/Review.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSiteProject2.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string ReviewTitle { get; set; }
+        public string ReviewContent { get; set; }
+        public DateTime PublishingDate { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Schema.ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
---
+++ ReviewSiteProject2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReviewSiteProject2.Startup))]
+namespace ReviewSiteProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
