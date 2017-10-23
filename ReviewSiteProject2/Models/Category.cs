eb2edf49b8234edcaee991f49abbed53fcea8e42
bradberger01 <bradberger@hotmail.com>
2017-10-23 14:00:22 -0400

    MVP

--- ReviewSiteProject2/Models/Category.cs
+++ ReviewSiteProject2/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,7 @@ namespace ReviewSiteProject2.Models
     {
         [Key]
         public int CategoryID { get; set; }
+        [DisplayName("Country")]
         public string CategoryName { get; set; }
         public virtual ICollection<Review> Review { get; set; }
     }
--- ReviewSiteProject2/Models/Review.cs
+++ ReviewSiteProject2/Models/Review.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace ReviewSiteProject2.Models
 {
+
     public class Review
     {
         [Key]
         public int ReviewID { get; set; }
+        [System.ComponentModel.DisplayName("Title")]
         public string ReviewTitle { get; set; }
-    
+        [DisplayName("Body")]
         public string ReviewContent { get; set; }
+        [DisplayName("Publishing Date")]
         public DateTime PublishingDate { get; set; }
 
         [System.ComponentModel.DataAnnotations.Schema.ForeignKey("Category")]
